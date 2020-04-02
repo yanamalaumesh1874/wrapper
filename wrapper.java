55bffa95e140ad202da7469fad6b4389a45a8c1b
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-02 20:20:45 +0530

    Create wrapper.java

---
+++ wrapper.java
@@ -0,0 +1,10 @@
+public class wrapper {
+  public static void main(String[] args) {
+    Integer a=5;
+    Double b=5.99;
+    Character c='A';
+    System.out.println(a.intValue());
+    System.out.println(b.doubleValue());
+    System.out.println(c.charValue());
+  }
+}
